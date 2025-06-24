f3424af2faa889ae7101d2524d642338ca76a752
Noe19 <denissecumbal2015@hotmail.com>
2025-06-23 22:39:15 -0500

    agregre enviar a la base

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const fetchData = require('./fetchData');
+const saveData01 = require('./saveData');
 const saveData = require('./saveData');
 
 async function main() {
   const data = await fetchData();
   if (data) {
-    saveData(data);
+   // saveData(data);
+   saveData01();
   }
 }
 
--- src/saveData.js
+++ src/saveData.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 const path = require('path');
-
+const db = require('./db');
 function saveData(data) {
   const filePath = path.join(__dirname, 'output.json');
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
   console.log('Datos guardados en', filePath);
+   saveData01()
+
+
+}
+
+async function saveData01(datos){
+     await db.query(
+              'INSERT INTO productos (id, nombre, precio,description) VALUES (?, ?, ?,?)',
+              ["10", "pancito001", "20.85","no nada"]
+            );
+
 }
 
 module.exports = saveData;
